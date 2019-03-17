7087fe009cde856cc2759fbfb62b438f04c05d59
Core-Build-Devops <bharathkumarkarthick@gmail.com>
2019-03-17 23:35:40 +0530

    baby

--- Website/index.js
+++ Website/index.js
@@ -1,55 +1,45 @@
 function sign() {
-    let user = document.getElementById("user").value;
-    let pas = document.getElementById("pass").value;
-    if ((user === "") || (pas === "")) {
-        alert("Enter userid and password")
-    }
-    if ((user === "Ramya") && (pas === "12345")) {
-        sessionStorage.setItem("storageName", "Ramya");
-        window.location.href = 'User.html';
-    }
+  let user = document.getElementById("user").value;
+  let pas = document.getElementById("pass").value;
+  if ((user === "") || (pas === "")) {
+    alert("Enter userid and password")
+  }
+  if ((user === "Ramya") && (pas === "12345")) {
+    sessionStorage.setItem("storageName", "Ramya");
+    window.location.href = 'User.html';
+  }
 }
-
-function sign() {
-    let user = document.getElementById("user").value;
-    let pas = document.getElementById("pass").value;
-
-    if ((user === "") || (pas === "")) {
-        alert("Enter userid and password")
-    }
-    if ((user === "Ramya") && (pas === "12345")) {
-        sessionStorage.setItem("storageName", "Ramya");
-        window.location.href = 'User.html';
-    }
-}
-
-
-
 
 function logout() {
-    sessionStorage.setItem("storageName", null);
-    alert("You are logged out")
-    window.location.href = 'index.html';
+  sessionStorage.clear("storageName");
+  alert("You are logged out")
+  window.location.href = 'index.html';
 }
 
 function signUp() {
-    let a = document.getElementById("pass").value
-    let b = document.getElementById("pass1").value
-    if (a === b) {
-        console.log("done")
-        alert("User created !!!")
-    }
-    else {
-        alert("Password did not match. \n Sign Up again !!!");
-        window.location.href = 'index.html';
-    }
+  let a = document.getElementById("pass").value
+  let b = document.getElementById("pass1").value
+  if (a === b) {
+    console.log("done")
+    alert("User created !!!")
+  }
+  else {
+    alert("Password did not match. \n Sign Up again !!!");
+    window.location.href = 'index.html';
+  }
 }
 
+function per() {
+
+  window.location.href = 'User2.html';
+  alert(`Finished aptitude test successfully`);
+
+}
 
 function evaluat() {
-    alert("Thanks. \n Your report is generated !!!");
-    // window.location.href = 'result.html';
-    document.getElementById("result").innerHTML = `
+  alert("Thanks. \n Your report is generated !!!");
+  // window.location.href = 'result.html';
+  document.getElementById("result").innerHTML = `
     
     <h2> Overall Pass % </h2>
 
@@ -94,9 +84,9 @@ function evaluat() {
 }
 
 
-function alertuser(){
+function alertuser() {
 
-    alert('Based on your result % \n Some jobs have been applied \n Thanks')
-    window.location.href = 'index.html';
+  alert('Based on your result % \n Some jobs have been applied \n Thanks')
+  window.location.href = 'index.html';
 
 }
